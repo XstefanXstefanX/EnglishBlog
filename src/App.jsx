5231d03e323d7dd745dae80e9a96ab486d9e9810
StefanBabalici <stefan.babalici2006@gmail.com>
2023-10-15 22:14:38 +0300

    Ahh

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import Nav from "./components/Navigation.jsx";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Articles from "./pages/Articles.jsx";
 import Article1 from "./pages/Article1.jsx";
@@ -8,7 +8,7 @@ import Article1 from "./pages/Article1.jsx";
 function App() {
   return (
     <div className="background-cover">
-      <BrowserRouter basename="/EnglishBlog/">
+      <HashRouter basename="/">
         <Routes>
           <Route path="/" element={<Nav />}>
             <Route index element={<Home />}></Route>
@@ -19,7 +19,7 @@ function App() {
             ></Route>
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </div>
   );
 }
